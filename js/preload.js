8c30ad365a385b0758d113abfc73f2334f19751b
sia <jwook486@gmail.com>
2022-08-19 18:50:03 +0900

    08190649

---
+++ js/preload.js
@@ -0,0 +1,33 @@
+function preloading (imageArray) {
+
+	let n = imageArray.length;
+
+	for (let i = 0; i < n; i++) {
+
+		let img = new Image();
+
+		img.src = imageArray[i];
+
+	}
+
+};
+
+
+
+preloading([
+
+	"../img_/news_on.jpg",
+    "../img_/com_on.jpg",
+    "../img_/ser_on.jpg",
+    "../img_/por_on.jpg",
+    "../img_/cb4.jpg",
+    "../img_/news.jpg",
+    "../img_/com.jpg",
+    "../img_/ser.jpg",
+    "../img_/por.jpg",
+    "../img_/msb.png",
+    "../img_/msw.png",
+    "../img_/por.jpg",
+    "../img_/comma.png"
+
+]);
