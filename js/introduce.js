ee2a84e1b4740c6f09000925222285e874a5396e
sia <jwook486@gmail.com>
2022-09-07 03:18:23 +0900

    09070319

--- js/introduce.js
+++ js/introduce.js
@@ -1,41 +1,86 @@
-$(function () {
 
-    var el = $('.intro'),
-    mac = $('#mac'),
-    offset = mac.offset(),
-    windowHeight = $(window).height();
-  
-    $(window).on('scroll', function () {
-  
-      $('h1').fadeOut(500);
-  
-      var windowTop = $(window).scrollTop(),
-      scrollPercent = (offset.top - windowTop) / offset.top,
-      scale = 'scale(' + scrollPercent + ')';
-  
-      el.css('transform', scale);
-  
-      if (windowTop >= 320) {
-        el.hide();
-      } else {
-        el.show();
-      }
-  
-      $('body').css('background-position-Y', -Math.round($(this).scrollTop() * 0.2) + 'px');
-  
-    //   $('.fasterContent').css('top', -Math($(this).scrollTop() + 2) + 'px');
-    });
-  
-    /*  hide and show side nav*/
-    // var nav = $('.nav'),
-    // navBut = $('.navBut');
-  
-    // navBut.click(function () {
-    //   if (nav.height() === 0) {
-    //     nav.stop().animate({ height: '55px', opacity: '1.0' }, 300);
-    //   } else {
-    //     nav.stop().animate({ height: '0', opacity: '0.0' }, 300);
-    //   }
-    // });
-  
+
+const { to, set, timeline, registerPlugin } = gsap;
+
+registerPlugin(ScrollTrigger);
+
+function initialiseElements() {
+  gsap.from(".hero-hardware", { transform: "scale(2.4)" });
+
+}
+
+initialiseElements();
+
+const zoomTimeline = () => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".outer",
+      pin: true,
+      scrub: true,
+      start: "start start",
+      
+    }
   });
+  tl.addLabel("start")
+    .to(".hero-hardware", {
+      transform: "scale(1)"
+    });
+  return tl;
+};
+
+
+
+const masterTimeline = gsap.timeline();
+masterTimeline.add(zoomTimeline);
+
+skrollr.init();
+
+
+
+const trigger = new ScrollTrigger.default({
+    trigger: {
+        // If the trigger should just work one time
+        once: true,
+        offset: {
+            // Set an offset based on the elements position, returning an
+            // integer = offset in px, float = offset in percentage of either
+            // width (when setting the x offset) or height (when setting y)
+            //
+            // So setting an yOffset of 0.2 means 20% of the elements height,
+            // the callback / class will be toggled when the element is 20%
+            // in the viewport.
+            element: {
+                x: 0,
+                y: (trigger, rect, direction) => {
+                    // You can add custom offsets according to callbacks, you
+                    // get passed the trigger, rect (DOMRect) and the scroll
+                    // direction, a string of either top, left, right or
+                    // bottom.
+                    return 0.3
+                }
+            },
+            // Setting an offset of 0.2 on the viewport means the trigger
+            // will be called when the element is 20% in the viewport. So if
+            // your screen is 1200x600px, the trigger will be called when the
+            // user has scrolled for 120px.
+            
+            // viewport: {
+            //     x: 0,
+            //     y: (trigger, frame, direction) => {
+            //         // We check if the trigger is visible, if so, the offset
+            //         // on the viewport is 0, otherwise it's 20% of the height
+            //         // of the viewport. This causes the triggers to animate
+            //         // 'on screen' when the element is in the viewport, but
+            //         // don't trigger the 'out' class until the element is out
+            //         // of the viewport.
+
+            //         // This is the same as returning Math.ceil(0.2 * frame.h)
+            //         return trigger.visible ? 0 : 0.2
+            //     }
+            // }
+        },
+        
+    },
+});
+
+trigger.add('[data-trigger');
