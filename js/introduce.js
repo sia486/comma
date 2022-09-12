256e1487396592c8f81b0e2a2bed8f8651935a08
sia <jwook486@gmail.com>
2022-09-13 00:28:01 +0900

    09131227

--- js/introduce.js
+++ js/introduce.js
@@ -39,7 +39,7 @@ skrollr.init();
 const trigger = new ScrollTrigger.default({
     trigger: {
         // If the trigger should just work one time
-        once: true,
+        once: false,
         offset: {
             // Set an offset based on the elements position, returning an
             // integer = offset in px, float = offset in percentage of either
@@ -132,6 +132,102 @@ const trigger1 = new ScrollTrigger.default({
 
 trigger1.add('[data-trigger1');
 
+const trigger2 = new ScrollTrigger.default({
+    trigger: {
+        // If the trigger should just work one time
+        once: false,
+        offset: {
+            // Set an offset based on the elements position, returning an
+            // integer = offset in px, float = offset in percentage of either
+            // width (when setting the x offset) or height (when setting y)
+            //
+            // So setting an yOffset of 0.2 means 20% of the elements height,
+            // the callback / class will be toggled when the element is 20%
+            // in the viewport.
+            element: {
+                x: 0,
+                y: (trigger, rect, direction) => {
+                    // You can add custom offsets according to callbacks, you
+                    // get passed the trigger, rect (DOMRect) and the scroll
+                    // direction, a string of either top, left, right or
+                    // bottom.
+                    return 0.2
+                }
+            },
+            // Setting an offset of 0.2 on the viewport means the trigger
+            // will be called when the element is 20% in the viewport. So if
+            // your screen is 1200x600px, the trigger will be called when the
+            // user has scrolled for 120px.
+            
+            // viewport: {
+            //     x: 0,
+            //     y: (trigger, frame, direction) => {
+            //         // We check if the trigger is visible, if so, the offset
+            //         // on the viewport is 0, otherwise it's 20% of the height
+            //         // of the viewport. This causes the triggers to animate
+            //         // 'on screen' when the element is in the viewport, but
+            //         // don't trigger the 'out' class until the element is out
+            //         // of the viewport.
+
+            //         // This is the same as returning Math.ceil(0.2 * frame.h)
+            //         return trigger.visible ? 0 : 0.2
+            //     }
+            // }
+        },
+        
+    },
+});
+
+trigger2.add('[data-trigger2');
+
+const trigger3 = new ScrollTrigger.default({
+    trigger: {
+        // If the trigger should just work one time
+        once: true,
+        offset: {
+            // Set an offset based on the elements position, returning an
+            // integer = offset in px, float = offset in percentage of either
+            // width (when setting the x offset) or height (when setting y)
+            //
+            // So setting an yOffset of 0.2 means 20% of the elements height,
+            // the callback / class will be toggled when the element is 20%
+            // in the viewport.
+            element: {
+                x: 0,
+                y: (trigger, rect, direction) => {
+                    // You can add custom offsets according to callbacks, you
+                    // get passed the trigger, rect (DOMRect) and the scroll
+                    // direction, a string of either top, left, right or
+                    // bottom.
+                    return 0.5
+                }
+            },
+            // Setting an offset of 0.2 on the viewport means the trigger
+            // will be called when the element is 20% in the viewport. So if
+            // your screen is 1200x600px, the trigger will be called when the
+            // user has scrolled for 120px.
+            
+            // viewport: {
+            //     x: 0,
+            //     y: (trigger, frame, direction) => {
+            //         // We check if the trigger is visible, if so, the offset
+            //         // on the viewport is 0, otherwise it's 20% of the height
+            //         // of the viewport. This causes the triggers to animate
+            //         // 'on screen' when the element is in the viewport, but
+            //         // don't trigger the 'out' class until the element is out
+            //         // of the viewport.
+
+            //         // This is the same as returning Math.ceil(0.2 * frame.h)
+            //         return trigger.visible ? 0 : 0.2
+            //     }
+            // }
+        },
+        
+    },
+});
+
+trigger3.add('[data-trigger3');
+
 var controller = new ScrollMagic.Controller();
 var timeline1 = new TimelineMax();
 
@@ -153,5 +249,4 @@ var scene_main = new ScrollMagic.Scene({
   duration: "1700px"
 })
   .setTween(timeline1)
-  .addTo(controller)
-  .addIndicators();
+  .addTo(controller);
