dd2921b55cbd574253b76377df916b8cf876d647
sia <jwook486@gmail.com>
2022-10-14 02:21:43 +0900

    10140221

---
+++ js/community.js
@@ -0,0 +1,247 @@
+ // NAVIGATION LOGO SCROLL TOP
+//  $('.logo').on('click', function (e) {
+//     e.preventDefault();
+//     $('.nav-toggle').removeClass('open');
+//     $('.menu-left').removeClass('collapse');
+//     $('html, body').animate({
+//       scrollTop: 0 },
+//     750, 'easeInOutQuad');
+//   });
+  // LINKS TO ANCHORS
+  $('a[href^="#"]').on('click', function (event) {
+  
+    var $target = $(this.getAttribute('href'));
+  
+    if ($target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: $target.offset().top },
+      750, 'easeInOutQuad');
+    }
+  });
+  
+  // TOGGLE HAMBURGER & COLLAPSE NAV
+  $('.nav-toggle').on('click', function () {
+    $(this).toggleClass('open');
+    $('.menu-left').toggleClass('collapse');
+  });
+  // REMOVE X & COLLAPSE NAV ON ON CLICK
+  $('.menu-left a').on('click', function () {
+    $('.nav-toggle').removeClass('open');
+    $('.menu-left').removeClass('collapse');
+  });
+  
+  // SHOW/HIDE NAV
+  
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+  
+  $(window).scroll(function (event) {
+    didScroll = true;
+  });
+  
+  setInterval(function () {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+  
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+  
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+  
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    // if (st >= 1) {
+    //   // Scroll Down
+    //   $('header').removeClass('show-nav').addClass('hide-nav');
+    //   $('.nav-toggle').removeClass('open');
+    //   $('.menu-left').removeClass('collapse');
+    //   $('.framecontainer').addClass('margin1');
+    // } else {
+    //   // Scroll Up
+    //   if (500 >= st) {
+    //     $('header').removeClass('hide-nav').addClass('show-nav');
+    //     $('.framecontainer').removeClass('margin1');
+    //   }
+    // }
+  
+    lastScrollTop = st;
+  }
+  // $(function(){
+//     $(window).scroll(function(){
+//       var winTop = $(window).scrollTop();
+//       if(winTop >= 30){
+//         $("header").addClass("sticky-header");
+//       }else{
+//         $("header").removeClass("sticky-header");
+//       }//if-else
+//     });//win func.
+//   });//ready func.
+  //# sourceURL=pen.js
+
+  var Cal = function(divId) {
+
+    //Store div id
+    this.divId = divId;
+  
+    // Days of week, starting on Sunday
+    this.DaysOfWeek = [
+      'Sun',
+      'Mon',
+      'Tue',
+      'Wed',
+      'Thu',
+      'Fri',
+      'Sat'
+    ];
+  
+    // Months, stating on January
+    this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+  
+    // Set the current month, year
+    var d = new Date();
+  
+    this.currMonth = d.getMonth();
+    this.currYear = d.getFullYear();
+    this.currDay = d.getDate();
+  
+  };
+  
+  // Goes to next month
+  Cal.prototype.nextMonth = function() {
+    if ( this.currMonth == 11 ) {
+      this.currMonth = 0;
+      this.currYear = this.currYear + 1;
+    }
+    else {
+      this.currMonth = this.currMonth + 1;
+    }
+    this.showcurr();
+  };
+  
+  // Goes to previous month
+  Cal.prototype.previousMonth = function() {
+    if ( this.currMonth == 0 ) {
+      this.currMonth = 11;
+      this.currYear = this.currYear - 1;
+    }
+    else {
+      this.currMonth = this.currMonth - 1;
+    }
+    this.showcurr();
+  };
+  
+  // Show current month
+  Cal.prototype.showcurr = function() {
+    this.showMonth(this.currYear, this.currMonth);
+  };
+  
+  // Show month (year, month)
+  Cal.prototype.showMonth = function(y, m) {
+  
+    var d = new Date()
+    // First day of the week in the selected month
+    , firstDayOfMonth = new Date(y, m, 1).getDay()
+    // Last day of the selected month
+    , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+    // Last day of the previous month
+    , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+  
+  
+    var html = '<table>';
+  
+    // Write selected month and year
+    html += '<thead><tr>';
+    html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+    html += '</tr></thead>';
+  
+  
+    // Write the header of the days of the week
+    html += '<tr class="days">';
+    for(var i=0; i < this.DaysOfWeek.length;i++) {
+      html += '<td>' + this.DaysOfWeek[i] + '</td>';
+    }
+    html += '</tr>';
+  
+    // Write the days
+    var i=1;
+    do {
+  
+      var dow = new Date(y, m, i).getDay();
+  
+      // If Sunday, start new row
+      if ( dow == 0 ) {
+        html += '<tr>';
+      }
+      // If not Sunday but first day of the month
+      // it will write the last days from the previous month
+      else if ( i == 1 ) {
+        html += '<tr>';
+        var k = lastDayOfLastMonth - firstDayOfMonth+1;
+        for(var j=0; j < firstDayOfMonth; j++) {
+          html += '<td class="not-current">' + k + '</td>';
+          k++;
+        }
+      }
+  
+      // Write the current day in the loop
+      var chk = new Date();
+      var chkY = chk.getFullYear();
+      var chkM = chk.getMonth();
+      if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+        html += '<td class="today">' + i + '</td>';
+      } else {
+        html += '<td class="normal">' + i + '</td>';
+      }
+      // If Saturday, closes the row
+      if ( dow == 6 ) {
+        html += '</tr>';
+      }
+      // If not Saturday, but last day of the selected month
+      // it will write the next few days from the next month
+      else if ( i == lastDateOfMonth ) {
+        var k=1;
+        for(dow; dow < 6; dow++) {
+          html += '<td class="not-current">' + k + '</td>';
+          k++;
+        }
+      }
+  
+      i++;
+    }while(i <= lastDateOfMonth);
+  
+    // Closes table
+    html += '</table>';
+  
+    // Write HTML to the div
+    document.getElementById(this.divId).innerHTML = html;
+  };
+  
+  // On Load of the window
+  window.onload = function() {
+  
+    // Start calendar
+    var c = new Cal("divCal");			
+    c.showcurr();
+  
+    // Bind next and previous button clicks
+    getId('btnNext').onclick = function() {
+      c.nextMonth();
+    };
+    getId('btnPrev').onclick = function() {
+      c.previousMonth();
+    };
+  }
+  
+  // Get element by id
+  function getId(id) {
+    return document.getElementById(id);
+  }
